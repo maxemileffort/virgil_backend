94bcb1e88985274aca71cc4131dfa41e40a34f19
Max Wood <maxemileffort@gmail.com>
2023-11-26 13:54:57 -0600

    registration validation working

--- wordpress/pratice_tests/email-validation.js
+++ wordpress/pratice_tests/email-validation.js
@@ -1,28 +1,3 @@
-// jQuery(document).ready(function($) {
-//     $('#user_email').blur(function() {
-//         var email = $(this).val();
-//         console.log(email);
-//         $.ajax({
-//             url: my_plugin_ajax.ajax_url,
-//             type: 'POST',
-//             data: {
-//                 'action': 'wp_ajax_check_email',
-//                 'user_email': email
-//             },
-//             success: function(response) {
-//                 if (response === 'exists') {
-//                     $('#user_email').next('.email-error').remove();
-//                     $('#user_email').after('<p class="email-error">That email address is in use already.</p>');
-//                     $('input[type="submit"]').attr('disabled', 'disabled');
-//                 } else {
-//                     $('.email-error').remove();
-//                     $('input[type="submit"]').removeAttr('disabled');
-//                 }
-//             }
-//         });
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     var userEmailInput = document.getElementById('user_email');
     var submitButton = document.getElementsByName('submit_single_user')[0];
